bfa55273979c3a5dca4bb02e3e656476b1f186d9
Tannny0203 <tanmaypatil156@gmail.com>
2025-06-13 23:36:06 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,63 +1,84 @@
-from turtle import Screen
-from snake import Snake
-from food import Food
-from scoreboard import Scoreboard
-import time 
-screen = Screen()
-screen.setup(width=600,height=600)
-screen.bgcolor("black")
-screen.title("SARPANNA")
-screen.tracer(0)
 
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text , text = "00:00")
+    title_label.config(text = "timer")
+    check_marks.config(text = "")
+    global reps
+    reps = 0
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer ():
+    global reps
+    reps +=1
 
-
-
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
-
-
-
-
-
-game_is_on =True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-
-    if snake.head.distance(food)<15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
+    work_sec = WORK_MIN*60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN*60
 
 
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor()<-280:
-        game_is_on = False
-        scoreboard.game_over()
+    if reps % 8 == 0:
+      count_down(long_break_sec)
+      title_label.config(text="Break" ,fg = RED)
+    elif reps %2 ==0:
+      count_down(short_break_sec)
+      title_label.config(text="Break"
+      ,fg = PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Break" ,
+        fg = GREEN)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
+    if count >  0:
+        global timer
+        timer = window.after(1000,count_down,count-1)
+    else:
+        start_timer()
+        mark = ""
+        for _ in range(math.floor(reps/2)):
+            mark += "✔"
+        check_marks.congif(text = mark)
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("pomodoro")
+window.config(padx = 100  , pady=50,bg=YELLOW )
 
-    for segment in snake.segments[1:]:
-       if snake.head.distance(segment)< 10:
-            game_is_on = False
-            scoreboard.game_over()
+
+title_label  = Label(text = "Timer",fg= GREEN , bg = YELLOW,font = (FONT_NAME,30,"bold"))
+title_label.grid(column = 1 , row = 0)
+canvas = Canvas (width =200 , height = 224 , bg = YELLOW , highlightthickness = 0  )
+tomato_img = PhotoImage(file = "tomato.png")
+canvas.create_image(100,112,image = tomato_img)
+timer_text = canvas.create_text(100,130,text = "00:00",fill = "white" , font = (FONT_NAME,35,"bold"))
+canvas.grid(column =1 , row = 1 )
+start_button = Button(text = "Start" , highlightthickness = 0 , command = start_timer)
+start_button.grid(column = 0 , row =2 )
 
 
 
-    snake.move()
+reset_button = Button(text = "Reset" , highlightthickness = 0 , command=reset_timer)
+reset_button.grid(column = 2 , row =2)
 
+check_marks = Label( fg = GREEN , bg = YELLOW )
+check_marks.grid(column = 1 , row = 3 )
+window.mainloop()
 
-
-
-
-
-
-
-
-
-
-
-screen.exitonclick()
